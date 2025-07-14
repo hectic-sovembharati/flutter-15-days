9f6cd3ab79b180fd3c6713dca69ec5c56d5c621c
Sovem Jung Bharati <your.email@example.com>
2025-07-14 21:24:55 +0545

    Input TextFiel in Day_5

---
+++ day_5/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Input Text'),
+        ),
+        body: Center(
+          child: TextInputWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class TextInputWidget extends StatelessWidget {
+  const TextInputWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min, // added to avoid layout issues
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Enter your text',
+          ),
+        ),
+      ],
+    );
+  }
+}
